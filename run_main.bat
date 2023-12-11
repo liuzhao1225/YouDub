3f6835cdd03bdad78dbe1dd0e5d123aefbd5329a
ZhaoLiu <liuzhao1225@gmail.com>
2023-12-11 17:01:57 +0800

    微信群

--- run_main.bat
+++ run_main.bat
@@ -1,1 +1,10 @@
-python main.py --input_folder input/TED_ED --output_folder output/TED_ED
+@echo off
+setlocal
+
+set "input_folders=input/z_Others input/CodeAesthetic"
+set "output_folders=output/z_Others output/CodeAesthetic"
+
+python main.py --input_folders %input_folders% --output_folders %output_folders% --diarize
+
+endlocal
+pause
